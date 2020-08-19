836fa3d8880946bcaf414adf1965936fd5f71a17
NicoRondan <nicolasrondan@live.com.ar>
2020-08-19 04:43:55 -0300

    inicio de proyecto

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./db/schema');
 const resolvers = require('./db/resolvers');
 
+const conectarDB = require('./config/db');
+
+//Conectar a la base de datos
+conectarDB();
+
 
 //Servidor
 const server = new ApolloServer({
-   
+   typeDefs,
+   resolvers
 });
 
 //Arrancar el servidor
